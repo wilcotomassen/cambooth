516620598c650fdfcc26ae54c7f1baf3d1157df1
Wilco Tomassen <wilco@wilcotomassen.nl>
2018-06-24 22:16:38 +0200

    Added quit shortcut

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,25 +4,29 @@
 #include "imagecarousel.h"
 
 #include <QShortcut>
+#include <QApplication>
 #include <QDebug>
-
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
 	// Open dialog on startup
-	//openSetupDialog();
+	openSetupDialog();
 
+	// Setup image carousel
 	imageCarousel = new ImageCarousel(this);
 	setCentralWidget(imageCarousel);
-
 
 	// Set fullscreen shortcut
 	QShortcut* toggleFullscreenShortcut = new QShortcut(QKeySequence(tr("Ctrl+F")), this);
 	toggleFullscreenShortcut->setContext(Qt::ApplicationShortcut);
 	connect(toggleFullscreenShortcut, SIGNAL(activated()), this, SLOT(toggleFullscreen()));
 
+	// Set quit shortcut
+	QShortcut* quitShortcut = new QShortcut(QKeySequence(tr("Ctrl+Shift+Q")), this);
+	quitShortcut->setContext(Qt::ApplicationShortcut);
+	connect(quitShortcut, SIGNAL(activated()), QApplication::instance(), SLOT(quit()));
 
-
+	this->setWindowState(Qt::WindowFullScreen);
 }
 
 MainWindow::~MainWindow() {
